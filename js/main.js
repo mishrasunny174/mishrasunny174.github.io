8d0947209e11d662a6e6c907b9f908eb2a00dced
mishrasunny174 <mishrasunny174@gmail.com>
2019-01-17 00:38:39 +0530

    made navbar responsive with dropdown menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggle () {
+    var x = document.getElementById("nav-links");
+    if(x.style.display==="none") {
+        x.style.display = "flex"
+    } else {
+        x.style.display = "none";
+    }
+}
