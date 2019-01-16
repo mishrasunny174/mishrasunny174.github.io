b396594a0267c7f4f0d6ec911be82cb04017a92a
mishrasunny174 <mishrasunny174@gmail.com>
2019-01-17 01:13:23 +0530

    fixed an annoying bug where nav links wont show uo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function toggle () {
     var x = document.getElementById("nav-links");
-    if(x.style.display==="none") {
-        x.style.display = "flex"
+    if(x.className==="normal-nav-links") {
+        x.className = "dropdown-nav-links";
     } else {
-        x.style.display = "none";
+        x.className = "normal-nav-links";
     }
 }
