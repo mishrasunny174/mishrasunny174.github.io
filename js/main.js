5ae78a28676ad242717b8680ebce45c8e1d9a1d2
mishrasunny174 <mishrasunny174@gmail.com>
2024-06-22 15:16:50 +0530

    Removed everything

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function toggle () {
-    var x = document.getElementById("nav-links");
-    if(x.className==="normal-nav-links") {
-        x.className += " dropdown-nav-links";
-    } else {
-        x.className = "normal-nav-links";
-    }
-}
